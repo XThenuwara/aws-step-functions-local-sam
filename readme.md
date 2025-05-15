61f0cf4a7b10718c5bb75e21b602d5b23cfd1430
xthenuwara <xthenuwara@gmail.com>
2025-05-15 11:58:16 +0530

    Lambda Test Function + Step Functions Env Setup

---
+++ readme.md
@@ -0,0 +1,6 @@
+# AWS Step Functions Local Test Using SAM
+
+## Run Step Functions Local
+```
+docker run -p 8083:8083 --env-file step-functions.env amazon/aws-stepfunctions-local
+```
---
+++ src/index.js
@@ -0,0 +1,9 @@
+
+// lambda test function
+export const handler = async (event) => {
+  const response = {
+    statusCode: 200,
+    body: JSON.stringify('Hello from Lambda!'),
+  };
+  return response;
+};
